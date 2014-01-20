14209c43435077735f756070db555266458ea5ed
Eric Rongo <erongo@qce.qualcomm.com>
2014-01-20 09:53:50 -0800

    Revert "AJTCT-70 fixed nvram function names"
    This reverts commit 483f4ddcfe0a7e669df923b87d1d7ed09cf1267c.
    
    Change-Id: I3be14922607c11596ab8e9690149b058d7ccc5b1
    Signed-off-by: Eric Rongo <erongo@qce.qualcomm.com>

--- src/aj_nvram.c
+++ src/aj_nvram.c
@@ -2,7 +2,7 @@
  * @file
  */
 /******************************************************************************
- * Copyright (c) 2013, 2014, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2013, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -302,9 +302,4 @@ uint8_t AJ_NVRAM_Exist(uint16_t id)
         return FALSE; // the unique id is not allowed to be 0
     }
     return (NULL != AJ_FindNVEntry(id));
-}
-
-void AJ_NVRAM_Clear()
-{
-    _AJ_NVRAM_Clear();
 }
