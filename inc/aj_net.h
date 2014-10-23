011f97d5ac911e875f6ae36a48f5eb2e95c74cba
Peter Krystad <pkrystad@qce.qualcomm.com>
2014-10-23 10:59:45 -0700

    ASACORE-777: mDNS for TCL/Router discovery
    Use NGNS (mDNS) to discover Routing Nodes.
    
    Send mDNS queries according to the new router discovery schedule. Listen for
    mDNS responses unicasted to the thin client.
    
    Legacy NS (version 1) queries are sent (and responses listened for) only when
    the minimum allowed router protocol version is set to allow connection to
    a legacy router.
    
    Change-Id: Ie14b8315843ca9e2ae5f85090fc11d166f136aab
    Signed-off-by: Peter Krystad <pkrystad@qce.qualcomm.com>

--- inc/aj_net.h
+++ inc/aj_net.h
@@ -54,18 +54,6 @@ AJ_Status AJ_Net_Connect(AJ_NetSocket* netSock, uint16_t port, uint8_t addrType,
 void AJ_Net_Disconnect(AJ_NetSocket* netSock);
 
 /**
- * Enable multicast data (for discover and advertising)
- *
- * @return        Return AJ_Status
- */
-AJ_Status AJ_Net_MCastUp(AJ_NetSocket* netSock);
-
-/**
- * Disable multicast data (for discover and advertising)
- */
-void AJ_Net_MCastDown(AJ_NetSocket* netSock);
-
-/**
  * Send from an I/O buffer
  *
  * @return        Return AJ_Status
@@ -78,6 +66,28 @@ AJ_Status AJ_Net_Send(AJ_IOBuffer* txBuf);
  * @return        Return AJ_Status
  */
 AJ_Status AJ_Net_Recv(AJ_IOBuffer* rxBuf, uint32_t len, uint32_t timeout);
+
+/**
+ * Abstracts discovery sockets
+ */
+typedef struct _AJ_MCastSocket {
+    AJ_IOBuffer tx;
+    AJ_IOBuffer rx;
+    uint32_t mDnsRecvAddr;
+    uint32_t mDnsRecvPort;
+} AJ_MCastSocket;
+
+/**
+ * Enable multicast (for discovery)
+ *
+ * @return        Return AJ_Status
+ */
+AJ_Status AJ_Net_MCastUp(AJ_MCastSocket* mcastSock);
+
+/**
+ * Disable multicast (for discovery)
+ */
+void AJ_Net_MCastDown(AJ_MCastSocket* mcastSock);
 
 /**
  * Function that signals AJ_Net_Recv() to bail out early if it
