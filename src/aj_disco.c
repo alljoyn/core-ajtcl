c7246570b10c70913c2b983b5f9353672908f73d
Peter Krystad <pkrystad@qce.qualcomm.com>
2015-01-08 13:56:33 -0800

    Trivial fix to debug log message
    Change-Id: Ifa77fe0a4a18ef0cbb14a47b61209f1208a51acd
    Signed-off-by: Peter Krystad <pkrystad@qce.qualcomm.com>

--- src/aj_disco.c
+++ src/aj_disco.c
@@ -2,7 +2,7 @@
  * @file
  */
 /******************************************************************************
- * Copyright (c) 2012-2014, AllSeen Alliance. All rights reserved.
+ * Copyright (c) 2012-2015, AllSeen Alliance. All rights reserved.
  *
  *    Permission to use, copy, modify, and/or distribute this software for any
  *    purpose with or without fee is hereby granted, provided that the above
@@ -1059,7 +1059,7 @@ AJ_Status AJ_Discover(const char* prefix, AJ_Service* service, uint32_t timeout)
                         if (!AJ_IsRoutingNodeBlacklisted(service)) {
                             goto _Exit;
                         } else {
-                            AJ_InfoPrintf(("AJ_Discover(): Skipping blacklisted Routing Node"));
+                            AJ_InfoPrintf(("AJ_Discover(): Skipping blacklisted Routing Node\n"));
                         }
                     }
                 }
@@ -1073,7 +1073,7 @@ AJ_Status AJ_Discover(const char* prefix, AJ_Service* service, uint32_t timeout)
                         if (!AJ_IsRoutingNodeBlacklisted(service)) {
                             goto _Exit;
                         } else {
-                            AJ_InfoPrintf(("AJ_Discover(): Skipping blacklisted Routing Node"));
+                            AJ_InfoPrintf(("AJ_Discover(): Skipping blacklisted Routing Node\n"));
                         }
                     }
                 }
