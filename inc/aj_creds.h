5cf111942e8875481c11101741b6c5f563519a1e
Kevin Kane <kkane@microsoft.com>
2015-07-28 10:26:16 -0700

    ASACORE-2231 Use EKUs to indicate certificate type
    Update certificate code in various places to use EKUs to indicate if
    a certificate is an identity, membership, or unrestricted
    certificate instead of a custom extension. Change certificate
    ingestion and validation methods to insist leaf certificates are
    not unrestricted.
    
    Add AJ_X509FreeDecodedCertificateChain to free the returned chain from
    AJ_X509DecodeCertificateChainPEM, instead of duplicating the
    implementation in multiple places.
    
    Add a DecodeAndVerifyCertificateChainTest to exercise only the
    certificate parsing code.
    
    Copy the client certificates and private key over from
    AuthListenerECDHETest in the standard client since they follow the
    correct EKU usage.
    
    Fix a bug in AJ_X509DecodeCertificatePEM where it didn't then
    decode the underlying DER and populate the rest of the structure.
    
    Change-Id: Iab940ea667439bc532ffab4597633d807424d12a
    Signed-off-by: Kevin Kane <kkane@microsoft.com>

--- inc/aj_creds.h
+++ inc/aj_creds.h
@@ -57,6 +57,8 @@ extern "C" {
 #define AJ_CERTIFICATE_OEM_X509     0x0000 /**< Manufacturer certificate */
 #define AJ_CERTIFICATE_IDN_X509     0x0100 /**< Identity certificate */
 #define AJ_CERTIFICATE_MBR_X509     0x0200 /**< Membership certificate */
+#define AJ_CERTIFICATE_UNR_X509     (AJ_CERTIFICATE_IDN_X509 | AJ_CERTIFICATE_MBR_X509) /**< Unrestricted certificate */
+#define AJ_CERTIFICATE_INV_X509     0x0400 /**< Invalid certificate (EKUs present but no AllJoyn EKUs) */
 #define AJ_CONFIG_CLAIMSTATE        0x0000 /**< Claim state */
 #define AJ_CONFIG_ADMIN_GROUP       0x0100 /**< Admin group identifier */
 
